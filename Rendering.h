c4ad90728811b516e3bf7490561c25511f58d310
Rev <64957743+Rev@users.noreply.github.com>
2022-03-02 21:49:32 +0800

    Create Rendering.h

---
+++ Rendering.h
@@ -0,0 +1,14 @@
+#include "../ImGui/imgui_internal.h"
+
+namespace Rendering {
+
+    void DrawText2(float fontSize, ImVec2 position, ImVec4 color, const char *text)
+    {
+        auto background = ImGui::GetBackgroundDrawList();
+        if(background)
+        {
+            background->AddText(NULL, fontSize, position, ImColor(color.x,color.y,color.z,color.w), text);
+        }
+    }
+
+}
